faf90486d586df83cdf18490f2dbb7e6589aa290
angeltveit <tan@scoutgg.com>
2024-04-23 17:01:52 +0200

    Stop using fs since cloudflare

--- src/routes/play/+page.server.js
+++ src/routes/play/+page.server.js
@@ -1,22 +1,5 @@
-import fs from 'fs';
-import path from 'path';
-
+import { cardValues } from './values.js'
 
 export async function load({ params }) {
-  const directoryPath = './static/images/cards';
-  try {
-    // Read the contents of the directory
-    const files = await fs.promises.readdir(directoryPath);
-
-    // Filter out only the files (exclude directories)
-    const fileList = files.filter(file => {
-      return fs.statSync(path.join(directoryPath, file)).isFile();
-    });
-
-    return { cards: fileList };
-  } catch (error) {
-    console.error('Error reading directory:', error);
-    throw error;
-  }
-
+    return { cards: Object.keys(cardValues) };
 }
